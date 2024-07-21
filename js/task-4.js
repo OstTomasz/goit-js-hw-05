eef4a2f7b66be9879174c9d788c6af1043f385ae
Tomasz Ostaszewski <ost.tomasz@gmail.com>
2024-07-21 17:00:36 +0200

    task 4 completed

--- js/task-4.js
+++ js/task-4.js
@@ -1,3 +1,50 @@
 console.log("Zadanie 4");
+const getTotalBalanceByGender = (users, gender) =>
+  users
+    .filter((user) => user.gender === gender)
+    .map((user) => user.balance)
+    .reduce((total, balance) => total + balance, 0);
+
+const allUsers = [
+  {
+    name: "Moore Hensley",
+    gender: "male",
+    balance: 2811,
+  },
+  {
+    name: "Sharlene Bush",
+    gender: "female",
+    balance: 3821,
+  },
+  {
+    name: "Ross Vazquez",
+    gender: "male",
+    balance: 3793,
+  },
+  {
+    name: "Elma Head",
+    gender: "female",
+    balance: 2278,
+  },
+  {
+    name: "Carey Barr",
+    gender: "male",
+    balance: 3951,
+  },
+  {
+    name: "Blackburn Dotson",
+    gender: "male",
+    balance: 1498,
+  },
+  {
+    name: "Sheree Anthony",
+    gender: "female",
+    balance: 2764,
+  },
+];
+
+console.log("male: ", getTotalBalanceByGender(allUsers, "male")); // 12053
+
+console.log("female: ", getTotalBalanceByGender(allUsers, "female")); // 8863
 
 console.log("---------------------");
